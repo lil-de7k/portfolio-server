ee1ff212c8a1fbebbc389aa073e118f93c144396
Muhammad <muhvmmvdessvm@gmail.com>
2025-04-26 16:16:42 +0300

    edit database

--- index.js
+++ index.js
@@ -24,14 +24,10 @@ app.use('/api/messages', messageRoutes);
 // Connect to MongoDB and start server
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-.then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch(err => {
-    console.error('MongoDB connection error:', err);
+const connectDB = require('./config/dbConfig');
+
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
 });
