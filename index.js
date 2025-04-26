7bb1501c229f959e8b3a9aa73078f46aa1f27d9d
Muhammad <muhvmmvdessvm@gmail.com>
2025-04-26 15:17:02 +0300

    from bcrypt to bcryptjs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 // Connect to MongoDB and start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, { 
     useNewUrlParser: true, 
